00e98c8df67d59df6a0699d86890abb285d34d51
shiniko <rodrialeix4@gmail.com>
2026-05-16 16:18:25 +0200

    Landing, Index, Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { Landing } from './pages/landing/landing';
 import { Inicio } from './pages/inicio/inicio';
 import { Calendario } from './pages/calendario/calendario';
+import { Creador } from './pages/creador/creador';
+import { Pomodoro } from './pages/pomodoro/pomodoro';
 
 export const routes: Routes = [
   { path: '', component: Landing },
   { path: 'inicio', component: Inicio },
   { path: 'calendario', component: Calendario },
+  { path: 'creador', component: Creador },
+  { path: 'pomodoro', component: Pomodoro },
   { path: '**', redirectTo: '' }
 ];
